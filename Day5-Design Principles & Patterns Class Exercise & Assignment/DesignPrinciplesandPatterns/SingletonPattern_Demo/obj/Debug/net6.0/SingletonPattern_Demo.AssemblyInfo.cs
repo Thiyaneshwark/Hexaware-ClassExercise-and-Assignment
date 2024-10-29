a749ffa7152e850ab751638b18be8d1357e29ae6
Thiyaneshwark <115340298+Thiyaneshwark@users.noreply.github.com>
2024-10-29 18:48:26 +0530

    Updated design pattern assignments with changes to project files and configuration settings

--- Day5-Design Principles & Patterns Class Exercise & Assignment/DesignPrinciplesandPatterns/SingletonPattern_Demo/obj/Debug/net6.0/SingletonPattern_Demo.AssemblyInfo.cs
+++ Day5-Design Principles & Patterns Class Exercise & Assignment/DesignPrinciplesandPatterns/SingletonPattern_Demo/obj/Debug/net6.0/SingletonPattern_Demo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SingletonPattern_Demo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cff2a141dca7c7041ca60d8b58b032773a7ca15f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0381aea0bc30c06ecd1336c5500f5521beb797dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SingletonPattern_Demo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SingletonPattern_Demo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
