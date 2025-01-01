0aeb17070093610cb266b417ebcaa42d112b7b73
Thiyaneshwark <115340298+Thiyaneshwark@users.noreply.github.com>
2025-01-01 15:54:02 +0530

    Add files via upload

---
+++ Day 21-React/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Department from "./Components/Departmentcrud";
+function App() {
+  return (
+    <div className="App">
+      <Department />
+    </div>
+  );
+}
+
+export default App;
